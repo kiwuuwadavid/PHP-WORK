88ede4f9568043b9f4ea9b06bf7ffd82028a54e6
kiwuuwadavid <kiwuuwadavid17@gmail.com>
2026-03-31 11:59:41 +0300

    Add files via upload

---
+++ php-work/Activity.php
@@ -0,0 +1,39 @@
+<?php
+
+$studentname = "David K";
+echo "My name is $studentname.";
+
+$schoolFees = 800000;
+echo "My school fees is $schoolfees.";
+
+
+$paid = true;
+$notpaid = false;
+$ugandandistrictarray = ["Wakiso", "Kampala", "Jinja", "Arua", "Kasese"]
+$anAssociativeArray = [
+    "name" => "David K",
+    "age" => 25,
+    "district" => "Wakiso",
+    "course" => "Web dev"
+];
+echo "My name is " . $anAssociativeArray["name"] . ", I am " . $anAssociativeArray["age"] . ", I am living in " . $anAssociativeArray["district"] . ", I am  years old, and I am studying " . $anAssociativeArray["course"] . ".";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
